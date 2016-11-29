97644897c369a1cdcbe4481c8b7930439510dc17
Creeland <cree@provinsal.com>
2016-11-28 16:38:22 -0800

    Converted master into a README for the course

--- src/home/theme/state/themeActionCreators.js
+++
@@ -1,8 +0,0 @@
-import {UPDATE_THEME_COLOR} from '../../../state/ActionTypes';
-
-export function updateThemeColor(payload) {
-  return {
-    type: UPDATE_THEME_COLOR,
-    payload: payload
-  };
-}
