a8b57fb8baad0f65492bcd6e83de9dbdb1869e79
Creeland <cree@provinsal.com>
2016-12-06 14:57:44 -0800

    Removed unnecessary tests and fixed lesson test

--- src/home/quote/components/LikeCounter.spec.js
+++ src/home/quote/components/LikeCounter.spec.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
-import likeCounter from './LikeCounter';
+import LikeCounter from './LikeCounter';
 
 describe('LikeCounter', () => {
 
-  it('should show the like count as active', function() => {
+  it('should show the like count as active', () => {
     const renderer = TestUtils.createRenderer();
     renderer.render(<LikeCounter count={5} isActive={true} />);
     const actual = renderer.getRenderOutput().props.className
@@ -14,7 +14,7 @@ describe('LikeCounter', () => {
     expect(actual).toEqual(expected);
   });
 
-  it('should show the like count as inactive', function() => {
+  it('should show the like count as inactive', () => {
     const renderer = TestUtils.createRenderer();
     renderer.render(<LikeCounter count={5} isActive={false} />);
     const actual = renderer.getRenderOutput().props.className
